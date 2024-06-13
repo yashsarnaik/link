1829194869bdc0396434383e8e6aca2eca005bb0
yash sarnaik <85468955+yashsarnaik@users.noreply.github.com>
2024-06-13 16:51:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ html_code = """
             </g>
         </g>
     </svg>
-  </a>
+  </a>&nbsp;
 
   <a href= 'https://www.instagram.com/yashsarnaik23/' target="_blank"  >
     <svg width="800px" height="800px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -85,12 +85,12 @@ html_code = """
         </g>
     </g>
 </svg>
-  </a>
+  </a>&nbsp;
   
 <a href= 'https://x.com/Yash_Sarnaik23' target="_blank">
       <?xml version="1.0" ?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" width="24px" height="24px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve"><path d="M14.095479,10.316482L22.286354,1h-1.940718l-7.115352,8.087682L7.551414,1H1l8.589488,12.231093L1,23h1.940717  l7.509372-8.542861L16.448587,23H23L14.095479,10.316482z M11.436522,13.338465l-0.871624-1.218704l-6.924311-9.68815h2.981339  l5.58978,7.82155l0.867949,1.218704l7.26506,10.166271h-2.981339L11.436522,13.338465z"/></svg>
     </svg>
-</a>
+</a>&nbsp;
 
 <div class="tooltip-container">
   <span class="tooltip">yashsarnaik2303@gmail.com</span>
