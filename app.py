85af3261d9822ba54dc366df89d0088490cc6ff6
yash sarnaik <85468955+yashsarnaik@users.noreply.github.com>
2024-06-18 13:22:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1225,7 +1225,7 @@ l1="""
     <div class="card__title">Job interest</div>
     <div class="card__subtitle">▸AI/ML Engineer ▸Data Scientist ▸LLM ▸Data Engineer ▸MLOps ▸Deep Learning ▸Neural Networks training ▸Analysis ▸Visualization.
     <p>
-    <a href="https://github.com/yashsarnaik/link/blob/aebcaed7543e4564a2d71c3a21117fefb2b2b76b/requirements.txt" target="_blank" rel="noopener noreferrer" style="text-decoration: none;">
+    <a href="https://drive.google.com/file/d/1AXAhDz8BinJZDNSxT0FooycagBjJa-MS/view?usp=drive_link" target="_blank" rel="noopener noreferrer" style="text-decoration: none;">
     <button class="button" type="button">
   <span class="button__text">My Resume</span>
   <span class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" id="bdd05811-e15d-428c-bb53-8661459f9307" data-name="Layer 2" class="svg"><path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path><path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path><path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path></svg></span>
